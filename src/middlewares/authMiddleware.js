533dd369bbe39577ed89aadf652bf7527b9cdb9a
noload <vaibhavkamble023@gmail.com>
2024-02-23 21:46:18 +0530

    authentication middleware added

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import { JWT_KEY } from "../config/serverConfig.js";
+
+export const authMiddleware = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      res.status(400).json({
+        success: false,
+        message: "Token Not Found",
+      });
+    }
+
+    const decode = jwt.verify(token, JWT_KEY);
+
+    req.user = { userId: decode.userId };
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong in auth middleware",
+      error: "Auth Failed",
+      error,
+    });
+  }
+};
