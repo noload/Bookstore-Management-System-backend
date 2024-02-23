4ac7a240d9efe9f724f646202dac8a5be8cdc758
noload <vaibhavkamble023@gmail.com>
2024-02-23 20:41:03 +0530

    signup route created

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,23 @@
+import userModel from "../model/userModel.js";
+
 export const signupController = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    const { email } = req.body;
+    const user = await userModel.findOne({ email });
+    if (user) {
+      res.status(400).json({
+        success: false,
+        message: "User Already Exist !. Try with another email",
+      });
+    }
+
+    const newUser = await userModel.create(req.body);
+    let userId = newUser._id;
+    res.status(200).json({
+      success: true,
+      message: "User Created Successfully",
+      userId,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
--- src/middlewares/userValidaionMiddleware.js
+++ src/middlewares/userValidaionMiddleware.js
@@ -1,19 +1,21 @@
 import zod from "zod";
 export const signupValidation = async (req, res, next) => {
   try {
-    const signupSchema = zod.object({
-      name: zod
-        .string({ required_error: "Name field is not menioned" })
-        .trim()
-        .min(3, { message: "Must be 3 or more characters long" }),
-      email: zod
-        .string({ required_error: "Email field is not mentioned" })
-        .email({ message: "Invalid Email address" })
-        .trim(),
-      password: zod
-        .string({ required_error: "Password Not Mentioned" })
-        .min(8, { message: "Must be 8 or more charater long" }),
-    });
+    const signupSchema = zod
+      .object({
+        name: zod
+          .string({ required_error: "Name field is not menioned" })
+          .trim()
+          .min(3, { message: "Must be 3 or more characters long" }),
+        email: zod
+          .string({ required_error: "Email field is not mentioned" })
+          .email({ message: "Invalid Email address" })
+          .trim(),
+        password: zod
+          .string({ required_error: "Password Not Mentioned" })
+          .min(8, { message: "Must be 8 or more charater long" }),
+      })
+      .strict();
 
     const { success, error } = signupSchema.safeParse(req.body);
     let errorMsg = "";
