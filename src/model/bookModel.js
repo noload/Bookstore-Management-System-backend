bf3ceb5112ae45614a92237a6a2583dbb8597ad2
noload <vaibhavkamble023@gmail.com>
2024-02-24 21:46:18 +0530

    added bookModel

---
+++ src/model/bookModel.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  author: {
+    type: String,
+    require: true,
+  },
+  ISBN: {
+    type: String,
+    require: true,
+  },
+  price: {
+    type: Number,
+    require: true,
+  },
+  quantity: {
+    type: Number,
+    require: true,
+  },
+  review: [
+    {
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+      comment: {
+        type: String,
+        require: [true, "Comment required"],
+      },
+      rating: {
+        type: Number,
+        requre: [true, "Raating is required"],
+      },
+    },
+  ],
+});
+
+export default mongoose.model("book", bookSchema);
