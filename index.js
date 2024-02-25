ca9e37be9ede5df47139b93e51b18869f3b68940
noload <vaibhavkamble023@gmail.com>
2024-02-25 19:16:29 +0530

    basic express session configuration done

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import morgan from "morgan";
 //module imports
 import appRouter from "./src/routes/index.js";
 import connectDB from "./src/config/db.js";
-import { PORT } from "./src/config/serverConfig.js";
-
+import { PORT, SESSION_KEY } from "./src/config/serverConfig.js";
+import session from "express-session";
 const app = express();
 
 //Database connection
@@ -17,6 +17,15 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+//basic session configuration
+app.use(
+  session({
+    secret: SESSION_KEY,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 //routers
 app.use("/api", appRouter);
 
--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -6,6 +6,7 @@ const configObj = {
   PORT: process.env.PORT,
   DB_URL: process.env.DB_URL,
   JWT_KEY: process.env.JWT_KEY,
+  SESSION_KEY: process.env.SESSION_KEY,
 };
 
-export const { PORT, DB_URL, JWT_KEY } = configObj;
+export const { PORT, DB_URL, JWT_KEY, SESSION_KEY } = configObj;
